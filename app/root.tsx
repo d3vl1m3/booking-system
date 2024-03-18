c4bc46e72e2f00e10ea688cb39dcbdb50234fc97
liam-harris <dev_lime@pm.me>
2024-03-18 10:01:53 +0000

    added TW typography plugin

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="prose">
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="leading-7">
+    <div>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
