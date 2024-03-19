857a06383a0b3850848f635f4c1f61a4a74e00b3
liam-harris <dev_lime@pm.me>
2024-03-19 09:52:37 +0000

    - add loader and data to booking list page

--- app/routes/bookings_+/index.tsx
+++ app/routes/bookings_+/index.tsx
@@ -1,21 +1,46 @@
-import { Link } from "@remix-run/react";
+import { Link, json, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+
+export function loader() {
+    const bookings = db.booking.getAll().map(({
+        id,
+        bookedBy,
+        dateEnd, 
+        dateStart, 
+        pets
+    }) => ({
+        id,
+        bookedBy,
+        dateEnd, 
+        dateStart, 
+        pets
+    }))
+    
+    return json({
+        bookings
+    })
+
+}
 
 export default function BookingsListPage() {
-    return <>
-        <h1>Bookings</h1>
-        <ul>
-            <li>
-                <Link to="./1">Booking 1</Link>
-            </li>
-            <li>
-                <Link to="./2">Booking 2</Link>
-            </li>
-            <li>
-                <Link to="./3">Booking 3</Link>
-            </li>
-            <li>
-                <Link to="./4">Booking 4</Link>
-            </li>
-        </ul>
-    </>
+    const { bookings } = useLoaderData<typeof loader>()
+    
+    return (
+        <>
+            <h1>Bookings</h1>
+            {bookings.length ? (
+                <ul>
+                    {bookings.map((booking) => (
+                        <li key={booking.id}>
+                            <Link to={`./${booking.id}`}>
+                                {`${booking.bookedBy?.name} (${booking.dateStart} to ${booking.dateEnd})`}
+                            </Link>
+                        </li>
+
+                    ))}
+                </ul>
+            ) : ('No Bookings')}
+        </>
+    )
 }
