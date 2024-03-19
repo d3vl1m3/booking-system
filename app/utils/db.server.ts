4e6838832cd463d912b9691fb081a4b127ea3b02
liam-harris <dev_lime@pm.me>
2024-03-19 09:36:21 +0000

    - add booking model and hydrate an example

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -4,8 +4,9 @@
  * the proper database.
  */
 import crypto from 'crypto'
-import { factory, manyOf, nullable, primaryKey } from '@mswjs/data'
+import { factory, manyOf, nullable, oneOf, primaryKey } from '@mswjs/data'
 import { singleton } from './singleton.server'
+import { invariantResponse } from '~/routes/users_+/misc'
 
 const getId = () => crypto.randomBytes(16).toString('hex').slice(0, 8)
 
@@ -26,6 +27,14 @@ export const db = singleton('db', () => {
 			createdAt: () => new Date(),
 
 			owners: manyOf('user'),
+		},
+		booking: {
+			id: primaryKey(getId),
+			createdAt: () => new Date(),
+			pets: manyOf('pet'),
+			dateStart: Date,
+			dateEnd: Date,
+			bookedBy: oneOf('user'),
 		},
 	})
 
@@ -70,6 +79,32 @@ export const db = singleton('db', () => {
 	for (const pet of pets) {
 		db.pet.create(pet)
 	}
+
+
+	function addDays(daysToAdd: number) {
+		const today = new Date();
+		const futureDate = new Date(today);
+		futureDate.setDate(today.getDate() + daysToAdd);
+		return futureDate;
+	}
+
+	const choji = db.pet.findFirst({
+		where: {
+			name: {
+				equals: 'Choji'
+			}
+		}
+	})
+
+	invariantResponse(choji, 'Choji was not found when hydrating the DB!')
+
+	db.booking.create({
+		id: '84jhsop947b2b2950kdsbnn383j2j2j2',
+		dateStart: addDays(3).toDateString(),
+		dateEnd: addDays(4).toDateString(),
+		bookedBy: userLiam,
+		pets: [choji]
+	})
 
 	return db
 })
