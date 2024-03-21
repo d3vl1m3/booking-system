ccd6bc8f4b59edd84938005185e901d35bd9b85c
liam-harris <dev_lime@pm.me>
2024-03-21 11:40:35 +0000

    wip: adding actions to the child route seems to be causing issues when the modal loads..

--- app/routes/pets+/_index+/_modals+/add/_addPetRoute.tsx
+++ app/routes/pets+/_index+/_modals+/add/_addPetRoute.tsx
@@ -1,7 +1,67 @@
-import { AddPetModal } from "~/routes/pets+/components/modals/addPetModal/addPetModal";
+import { ActionFunctionArgs, json, redirect } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { AddPetModal } from '~/routes/pets+/components/modals/addPetModal/addPetModal'
+import { db } from '~/utils/db.server'
+import { invariantResponse } from '~/utils/misc'
+
+export type Owner = {
+	id: string
+	name: string | null
+}
+
+export function loader() {
+	const owners = db.user
+		.getAll()
+		.map(({ id, name, username }) => ({ id, name, username }))
+
+	return json({ owners })
+}
+
+export async function actions({ request }: ActionFunctionArgs) {
+	const formData = await request.formData()
+	const formNameValue = formData.get('name')
+	const name = typeof formNameValue === 'string' ? formNameValue : undefined
+	const ownerId = formData.get('owner')
+
+	invariantResponse(name, 'Pet name required', { status: 422 })
+
+	invariantResponse(
+		typeof name !== 'string',
+		`Pet name has invlaid value: ${typeof name !== 'string'}`,
+		{ status: 422 },
+	)
+
+	invariantResponse(
+		typeof ownerId !== 'string',
+		`Owner ID has invlaid value: ${typeof ownerId !== 'string'}`,
+		{ status: 422 },
+	)
+
+	invariantResponse(ownerId, 'Owner ID required', { status: 422 })
+
+	const owner =
+		typeof ownerId === 'string'
+			? db.user.findFirst({
+					where: {
+						id: {
+							equals: ownerId,
+						},
+					},
+			  })
+			: null
+
+	invariantResponse(owner, 'Owner not found')
+
+	db.pet.create({
+		name,
+		owners: [owner],
+	})
+
+	// return redirect('/pets/')
+}
 
 export default function AddPetRoute() {
-    return (
-        <AddPetModal/>
-    )
+	const { owners }: { owners: Owner[] } = useLoaderData<typeof loader>()
+
+	return <AddPetModal owners={owners} />
 }
--- app/routes/pets+/components/modals/addPetModal/addPetModal.tsx
+++ app/routes/pets+/components/modals/addPetModal/addPetModal.tsx
@@ -1,10 +1,30 @@
-import { RouteBasedModal } from '~/components/RouteBasedModal/routeBasedModal';
+import { Form } from '@remix-run/react'
+import { RouteBasedModal } from '~/components/RouteBasedModal/routeBasedModal'
+import { type Owner } from '~/routes/pets+/_index+/_modals+/add/_addPetRoute'
 
-
-export const AddPetModal = () => {
-    return (
-        <RouteBasedModal>
-            <h1>Add Pet</h1>
-        </RouteBasedModal>
-    )
+export const AddPetModal = ({ owners }: { owners: Owner[] }) => {
+	return (
+		<RouteBasedModal>
+			<h1>Add Pet</h1>
+			<Form method="POST" action="./?index">
+				<div>
+					<label htmlFor="name">Name: </label>
+					<input type="text" name="name" id="name" />
+				</div>
+				<div>
+					<label htmlFor="owner">Owner: </label>
+					<select name="owner" id="owner" required>
+						{owners.map(owner => (
+							<option key={owner.id} value={owner.id}>
+								{owner.name}
+							</option>
+						))}
+					</select>
+				</div>
+				<button type="submit" className="btn">
+					Save
+				</button>
+			</Form>
+		</RouteBasedModal>
+	)
 }
