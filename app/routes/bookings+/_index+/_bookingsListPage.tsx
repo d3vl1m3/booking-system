ebebe2f77a27565d54572f5cd60c4fcc5f28d750
liam-harris <dev_lime@pm.me>
2024-03-22 11:45:59 +0000

    fix: updated to use the userId, not the username

--- app/routes/bookings+/_index+/_bookingsListPage.tsx
+++ app/routes/bookings+/_index+/_bookingsListPage.tsx
@@ -1,71 +1,57 @@
-import { Link, json, useLoaderData } from "@remix-run/react";
-import { db } from "~/utils/db.server";
-
+import { Link, json, useLoaderData } from '@remix-run/react'
+import { db } from '~/utils/db.server'
 
 export function loader() {
-    const bookings = db.booking.getAll().map(({
-        id,
-        bookedBy,
-        bookingRefrence,
-        dateEnd, 
-        dateStart, 
-        pets
-    }) => ({
-        id,
-        bookedBy,
-        bookingRefrence,
-        dateEnd, 
-        dateStart, 
-        pets
-    }))
-    
-    return json({
-        bookings
-    })
+	const bookings = db.booking
+		.getAll()
+		.map(({ id, bookedBy, bookingRefrence, dateEnd, dateStart, pets }) => ({
+			id,
+			bookedBy,
+			bookingRefrence,
+			dateEnd,
+			dateStart,
+			pets,
+		}))
 
+	return json({
+		bookings,
+	})
 }
 
 export default function BookingsListPage() {
-    const { bookings } = useLoaderData<typeof loader>()
-    
-    return (
-        <>
-            <h1>Bookings</h1>
-            {bookings.length ? (
-                <table>
-                    <tr>
-                        <th>Booking ref</th>
-                        <th>Booked by</th>
-                        <th>Pets booked</th>
-                        <th>Date start</th>
-                        <th>Date end</th>
-                    </tr>
-                {bookings.map((booking) => (
-                        <tr key={booking.id}>
-                            <td>
-                                <Link to={`./${booking.id}`}>
-                                    {booking.bookingRefrence}
-                                </Link>
-                            </td>
-                            <td>
-                                <Link to={`/users/${booking.bookedBy?.username}`}>
-                                    {booking.bookedBy?.name}
-                                </Link>
-                            </td>
-                            <td>
-                                {booking.pets.map(({name}) => name).join(', ')}
-                            </td>
-                            <td>
-                                {booking.dateStart}
-                            </td>
-                            <td>
-                                {booking.dateEnd}
-                            </td>
-                        </tr>
+	const { bookings } = useLoaderData<typeof loader>()
 
-                    ))}
-                </table>
-            ) : ('No Bookings')}
-        </>
-    )
+	return (
+		<>
+			<h1>Bookings</h1>
+			{bookings.length ? (
+				<table>
+					<tr>
+						<th>Booking ref</th>
+						<th>Booked by</th>
+						<th>Pets booked</th>
+						<th>Date start</th>
+						<th>Date end</th>
+					</tr>
+					{bookings.map(booking => (
+						<tr key={booking.id}>
+							<td>
+								<Link to={`./${booking.id}`}>{booking.bookingRefrence}</Link>
+							</td>
+							<td>
+								<Link to={`/users/${booking.bookedBy?.id}`}>
+									{booking.bookedBy?.name}
+								</Link>
+							</td>
+							<td>{booking.pets.map(({ name }) => name).join(', ')}</td>
+							<td>{booking.dateStart}</td>
+							<td>{booking.dateEnd}</td>
+						</tr>
+					))}
+				</table>
+			) : (
+				'No Bookings'
+			)}
+		</>
+	)
 }
