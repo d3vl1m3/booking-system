ae292772de0d77bdf42735a30ca99a39782ab49a
liam-harris <dev_lime@pm.me>
2024-03-22 11:32:58 +0000

    feat: update users list template on routes for add

--- app/routes/users+/_index+/_usersListPage.tsx
+++ app/routes/users+/_index+/_usersListPage.tsx
@@ -1,36 +1,40 @@
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from '@remix-run/react'
 
-import { db } from "app/utils/db.server"
-import { json } from "@remix-run/node";
+import { db } from 'app/utils/db.server'
+import { json } from '@remix-run/node'
 
 type User = {
-    id: string;
-    name: string | null;
-    username: string;
+	id: string
+	name: string | null
+	username: string
 }
-
 
 export async function loader() {
 	const users: User[] = db.user
-        .getAll()
-		.map(({ id, name, username }) => ({ id, name, username}))
+		.getAll()
+		.map(({ id, name, username }) => ({ id, name, username }))
 	return json({ users })
 }
 
 export default function UsersListPage() {
-	const { users }: {users: User[]} = useLoaderData<typeof loader>()
-    
+	const { users }: { users: User[] } = useLoaderData<typeof loader>()
 
-    return (
-        <>
-            <h1>List page</h1>
-            <ul>
-                {users.map((user) => (
-                    <li key={user.id}>
-                        <Link to={`./${user.username}`}>{user.name}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
-} 
+	return (
+		<>
+			<h1>List page</h1>
+			<ul>
+				<li>
+					<Link to="./add">Add User</Link>
+				</li>
+			</ul>
+			<ul>
+				{users.map(user => (
+					<li key={user.id}>
+						<Link to={`./${user.id}`}>{user.name}</Link>
+					</li>
+				))}
+			</ul>
+			<Outlet />
+		</>
+	)
+}
