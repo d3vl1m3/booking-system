4c7b199b8920cc69d09c1612dde7ce1f23ac592c
liam-harris <dev_lime@pm.me>
2024-03-27 09:49:49 +0000

    lint: auto linter changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,73 +1,64 @@
 import ReactModal from 'react-modal'
-import { LinksFunction } from "@remix-run/node";
+import { LinksFunction } from '@remix-run/node'
 import {
-  Link,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+	Link,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from '@remix-run/react'
 
 import TailwindCssUrl from '~/styles/tailwind.css?url'
 
-
 ReactModal.setAppElement('#app')
 
-
 export const links: LinksFunction = () => [
-  {
-    rel:'stylesheet', href: TailwindCssUrl
-  }
+	{
+		rel: 'stylesheet',
+		href: TailwindCssUrl,
+	},
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body id="app">
-        <header>
-          <Link className="heading-1" to="/">
-            Pet booker
-          </Link>
-          <nav>
-            <ul>
-              <li>
-                <Link to={"users/me"}>
-                  My profile
-                </Link>
-              </li>
-              <li>
-                <Link to={"users"}>
-                  Users
-                </Link>
-              </li>
-              <li>
-                <Link to={"pets"}>
-                  Pets
-                </Link>
-              </li>
-              <li>
-                <Link to={"bookings"}>
-                  Bookings
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body id="app">
+				<header>
+					<Link className="heading-1" to="/">
+						Pet booker
+					</Link>
+					<nav>
+						<ul>
+							<li>
+								<Link to={'users/me'}>My profile</Link>
+							</li>
+							<li>
+								<Link to={'users'}>Users</Link>
+							</li>
+							<li>
+								<Link to={'pets'}>Pets</Link>
+							</li>
+							<li>
+								<Link to={'bookings'}>Bookings</Link>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	)
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />
 }
